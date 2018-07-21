b1c15a5a60708b4db11661f1fa22f5847bcf95e5
Eponick <mikehuntwife@gmail.com>
2018-07-20 23:17:49 -0400

    testing encryption

--- app.rb
+++ app.rb
@@ -1,10 +1,21 @@
 require 'sinatra'
 require 'mysql2'
-if File.exist?('local_env.rb')
-  require_relative 'local_env.rb'
-end
-
+require_relative 'database.rb'
+enable :sessions
 
 get '/' do
-  ENV['INSTANCE_LOC']
+  erb :test_user
 end
+
+post '/create_user' do
+  uname = params[:username]
+  pass = params[:password]
+
+  db = DataBase.new
+  db.connect
+  result = db.client.query("INSERT INTO users (`name`, `password`) VALUES (AES_ENCRYPT('#{uname}', #{ENV['AES_KEY']}), AES_ENCRYPT('#{pass}', #{ENV['AES_KEY']}))")
+  p result.first
+  redirect '/'
+end
+
+# SELECT CAST(AES_DECRYPT(name, AES_KEY) AS CHAR(50)) FROM `users` WHERE id=1
