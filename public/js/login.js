d6345e1c9855969d4979e0a9e38b362a26211f32
Eponick <mikehuntwife@gmail.com>
2018-07-22 19:55:15 -0400

    Added cool new user transition

--- public/js/login.js
+++ public/js/login.js
@@ -4,16 +4,24 @@ function newUser()
   var loginBtn = document.getElementById("login_button");
   var newBtn = document.getElementById("new_button");
 
-  if(confirmField.type == "hidden")
+  if(confirmField.style.height.length < 1)
   {
-    confirmField.type = "password";
+    confirmField.style.height = "0em";
+  }
+  
+  if(confirmField.style.height == "0em")
+  {
+    confirmField.style.height = "2em";
+    confirmField.style.visibility = "visible";
     loginBtn.value = "Sign Up";
     newBtn.value = "Cancel";
     confirmField.required = true;
   }
   else
   {
-    confirmField.type = "hidden";
+    confirmField.style.height = "0em";
+    confirmField.style.visibility = "hidden";
+    confirmField.value = "";
     loginBtn.value = "Login";
     newBtn.value = "New User";
     confirmField.required = false;
