d3f9b34e3016bf621d97a7826eae4d88748536dc
Gary Boyd <eponick@gmail.com>
2018-07-25 12:55:47 -0400

    Added some js and save button

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,27 @@
-enableInput(name)
+function enableInput(name)
 {
     var field = document.getElementById(`contact-${name}`);
-    field.disabled = false;
+    var btn = document.getElementById(`edit-${name}`);
+    btn.hidden = true;
+    field.removeAttribute("disabled");
+    if(name != "notes")
+    {
+        field.addEventListener("keydown", function(e) {
+            if(e.which == 13 || e.keyCode == 13)
+            {
+                disableInput(name);
+                return false;
+            }
+            return true;
+        });
+    }
     field.focus();
+}
+
+function disableInput(name)
+{
+    var field = document.getElementById(`contact-${name}`);
+    var btn = document.getElementById(`edit-${name}`);
+    btn.hidden = false;
+    field.setAttribute("disabled", "true");
 }
